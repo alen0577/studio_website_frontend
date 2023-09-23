b0800f692c6c288c846b01aba20d3cff419894e3
Alen Antony <alenantony32@gmail.com>
2023-09-23 20:22:17 +0530

    4th commit

--- studio_app/views.py
+++ studio_app/views.py
@@ -8,4 +8,22 @@ def signin(request):
     return render(request,'login.html')  
 
 def signup(request):
-    return render(request,'register.html')       
+    return render(request,'register.html')  
+
+def about(request):
+    return render(request,'about.html')  
+
+def contact(request):
+    return render(request,'contact.html')  
+
+def services(request):
+    return render(request,'services.html') 
+
+def joinus(request):
+    return render(request,'joinus.html') 
+
+def booking(request):
+    return render(request,'booking.html')                         
+
+def gallery(request):
+    return render(request,'gallery.html')           
