44d27739a9ad243febf630fb8a2bd114423d0616
Alen Antony <alenantony32@gmail.com>
2023-09-26 20:46:16 +0530

    6th commit

--- studio_app/urls.py
+++ studio_app/urls.py
@@ -7,11 +7,11 @@ urlpatterns = [
     path('', views.index, name='index' ),
     path('signin/',views.signin,name='signin'),
     path('signup/',views.signup,name='signup'),
-    path('gallery/',views.gallery,name='gallery'),
+    path('recent_works/',views.gallery,name='gallery'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
     path('services/',views.services,name='services'),
     path('joinus/',views.joinus,name='joinus'),
-    path('booking/',views.booking,name='booking'),
+    path('request_services/',views.booking,name='booking'),
 
 ]     
--- studio_app/views.py
+++ studio_app/views.py
@@ -14,7 +14,7 @@ def about(request):
     return render(request,'about.html')  
 
 def contact(request):
-    return render(request,'contact.html')  
+    return render(request,'contactus.html')  
 
 def services(request):
     return render(request,'services.html') 
